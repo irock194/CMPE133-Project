c36ca364cf7303ad3c8ea9e6090e2ceb8e97738a
Gabriel Tenocelotl <47368091+irock194@users.noreply.github.com>
2020-11-17 14:56:47 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<div class="login_content">
+    <div class="inner_login_content">
+        <div class="login_tile">
+            <div class="login_title">
+              <u1 class="list">
+                  <li>Customer Login</li>
+                  <li><input type="text" name="User Name" placeholder="User Name"></li>
+                  <li><input type="password" name="Password" placeholder="......"></li>
+                  <li><input type="button" name="Submit" value="Submit"></li>
+              <u1>
+            </div>
+          </div>
+    <div class="register_tile">
+      <div class="login_tile">
+          <div class="login_title">
+            <u1 class="list">
+                <li>Register</li>
+                <li><input type="text" name="User Name" placeholder="User Name"></li>
+                <li><input type="password" name="Password" placeholder="Password"></li>
+                <li><input type="button" name="Submit" value="Register"></li>
+            <u1>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
